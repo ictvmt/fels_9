72b8256eaf6c8f717fa2d5090305b3b755da0468
tung <ictvmt1@gmail.com>
2014-07-21 10:03:43 +0700

    showActivities

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   
   def show
-    
+    @user = User.find(params[:id])
+    @activities = @user.activities.paginate(page: params[:page])
   end
 
   def new
---
+++ app/models/activities.rb
@@ -0,0 +1,4 @@
+class Activities < ActiveRecord::Base
+	belongs_to :user
+	validates :user_id, presence: true
+end
